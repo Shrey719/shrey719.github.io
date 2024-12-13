62eeb831c3891d701ba63a5ab60f51c93f03e55a
theskid22 <shrey150436@gmail.com>
2024-12-13 16:58:03 -0500

    fixer upper

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-let app = express()
-
-app.use(express.static("static"))
-app.use(express.static("assets")) 
-
-app.listen(8080);
-console.log("Listening on port 8080")
