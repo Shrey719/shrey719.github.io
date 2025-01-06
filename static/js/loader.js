1c910ef9d4471534b5cced79d3455a3601a01bd2
Shrey <164104873+Shrey719@users.noreply.github.com>
2025-01-06 15:42:26 +0000

    uh. stuff

---
+++ static/js/loader.js
@@ -0,0 +1,12 @@
+function LoadHome() {
+    document.getElementById("card").style.visibility = "visible" // lmao 
+    document.getElementById("blog").style.visibility = "hidden"
+}
+
+function LoadBlog() {
+    document.getElementById("card").style.visibility = "hidden"
+    document.getElementById("blog").style.visibility = "visible" 
+}
+function thing1() {
+    window.location.href = "/thingone.html"
+}
