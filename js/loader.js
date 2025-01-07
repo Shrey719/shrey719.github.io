7e2408aeb9bb5bae30e6e9518afe0087cdd0963b
Shrey719 <shrey150436@gmail.com>
2025-01-07 17:12:16 -0500

    that was..

--- js/loader.js
+++ js/loader.js
@@ -6,28 +6,4 @@ function LoadHome() {
 function LoadBlog() {
     document.getElementById("card").style.visibility = "hidden"
     document.getElementById("blog").style.visibility = "visible" 
-    const elements = document.querySelectorAll('.bloge');
-    elements.forEach(element => {
-      element.style.visibility = 'visible';
-    });
-}
-
-function loadBlog(blogFile) {
-  const be = document.querySelectorAll('.bloge');
-  be.forEach(element => {
-    element.style.visibility = 'hidden';
-  });
-    fetch(blogFile)  // Fetch the file from the root of the server
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.text(); 
-  })
-  .then(data => {
-    document.getElementById("blogSpace").innerText = data
-  })
-  .catch(error => {
-    console.error('Could not fetch blog (my code be shite):', error);
-  });
 }
